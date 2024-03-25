84f2070dd61e7d4d62995c16cf4ffd8955c30253
lestersadang <lestermillaresadang@gmail.com>
2024-03-25 13:29:38 +0800

    first coms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Demo</title>
-</head>
-<body>
-    <h1>Laravel Demo Page 1</h1>
-</body>
-</html>
+@include('partials.header')
+<h1>Laravel Demo Page 1</h1>
+@include('partials.footer')
